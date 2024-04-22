ca6876f85f061a96e876bdd1111c19f52e1b2d93
Luan Carlos <luancarlos@Luan-MacBook-M1.local>
2024-04-22 15:51:03 -0300

    atualixando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+import api from './services/api';
+import './styles.css';
 
 function App() {
+
+  // Funcao Input sai com está sem valor é ganha uma valor na parte de baixo
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    // 72120400/json/
+    if (input === '') {
+      alert("Preencha Algum CEP !")
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("");
+
+    } catch {
+      alert("Ops erro ao buscar! ");
+      setInput("")
+    }
+  }
+
+  function handleClear() {
+    setCep({}); // Define o estado 'cep' como um objeto vazio para limpar as informações
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>Complemento : {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+
+          <button onClick={handleClear} style={{
+            background: 'firebrick',
+            color: 'white',
+            fontSize: '15px',
+             padding: '10px 20px' 
+          }}>
+            Limpar
+          </button>
+        </main>
+      )
+      }
+    </div >
   );
 }
-
 export default App;
